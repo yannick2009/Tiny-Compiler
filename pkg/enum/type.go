c98ed1a89cba5bc467162cf7ef0b0c95d0b76429
yannick2009 <yannickkouakou13@gmail.com>
2025-01-11 16:01:23 +0100

    refactor: refactor the tokenMapType

--- pkg/enum/type.go
+++ pkg/enum/type.go
@@ -8,16 +8,15 @@ const (
 	Letter
 )
 
-
 // tokenTypeMap is a map that associates TokenType constants with their string representations.
 // This map is used to convert token types to their corresponding string names for easier identification and debugging.
 // The keys are of type TokenType and the values are their respective string names.
 var tokenTypeMap map[TokenType]string = map[TokenType]string{
-	Paren:         "Paren",
-	Number:        "NumberLiteral",
-	Letter:        "Name",
+	Paren:  "Paren",
+	Number: "NumberLiteral",
+	Letter: "CallExpression",
 }
 
-func (t TokenType) string() string {
+func (t TokenType) String() string {
 	return tokenTypeMap[t]
 }
