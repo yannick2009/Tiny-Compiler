0accd540a69dc6f00d4cec77a3b324a71f738b68
yannick2009 <yannickkouakou13@gmail.com>
2025-01-16 13:58:34 +0100

    docs: enrich comments for Node and Token structures in types package

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -2,7 +2,8 @@ package types
 
 import "tiny-compiler/pkg/enum"
 
-// Node represents a single node in the Abstract Syntax Tree (AST).
+// Node represents a single node in the Abstract Syntax Tree (AST). 
+// It consists of a node type (Kind), a node value (Value), a node name (Name), and a list of child nodes (Body).
 type Node struct {
 	Kind       string
 	Value      string
@@ -18,8 +19,13 @@ type Node struct {
 // AST represents the Abstract Syntax Tree (AST) of the input tokens.
 type AST Node
 
-// Token represents a single token in the input string.
+// Token represents a single token in the input string. It consists of a token type (Kind) and a token value (Value).
+// The token type is defined in the TokenType enum, and the token value is the actual character or string that the token represents.
 type Token struct {
 	Kind  enum.TokenType
 	Value string
 }
+
+// Visitor is a map of node types to visitor functions. It is used to traverse the AST and apply a function to each node.
+// n is the current node being visited, and p is the parent node of n.
+type Visitor map[string]func(n *Node, p Node)
